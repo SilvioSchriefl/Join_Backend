81b3d495aa7f5b1422af8502a5ddfd7d32470462
Silvio Schriefl <125505936+SilvioSchriefl@users.noreply.github.com>
2023-11-19 16:01:30 +0100

    add cors url

--- join_backend/join_backend/settings.py
+++ join_backend/join_backend/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOWED_ORIGINS = ['http://localhost:4200', 'http://localhost:58185', 'http://localhost:58417']
+CORS_ALLOWED_ORIGINS = ['http://localhost:4200', 'http://localhost:58185', 'http://localhost:58417', 'https://join.silvio-schriefl.de']
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
